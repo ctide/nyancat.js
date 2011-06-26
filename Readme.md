09fb286e2c1733424cc1d7d6da106ef13dc52de7
Nifty Lettuce <niftylettuce@gmail.com>
2011-06-26 07:00:40 -0400

    rainbows locked n loaded

---
+++ Readme.md
@@ -0,0 +1,42 @@
+<h1>nyancat.js - terminal/console animation of nyancat's rainbow using colors.js</h1>
+
+<img src="http://i.imgur.com/occaQ.png" alt="nyancat.js screenshot" />
+
+<h2>first and most importantly, click play on the video below</h2>
+<object width="425" height="349"><param name="movie" value="http://www.youtube.com/v/QH2-TGUlwu4?version=3&amp;hl=en_US"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/QH2-TGUlwu4?version=3&amp;hl=en_US" type="application/x-shockwave-flash" width="425" height="349" allowscriptaccess="always" allowfullscreen="true"></embed></object>
+
+<h2>ensure you have the latest version of <a href="http://nodejs.org" target="_blank">node.js</a> and <a href="http://npmjs.org/" target="_blank">npm</a> installed</h2>
+
+<h2>then simply run the following commands from terminal to get started!</h2>
+
+      npm install colors
+      node nyancat.js
+
+<h3><span style="background-color: yellow">hint:</span> resize your terminal window and run `node nyancat.js` again ;)</h3>
+
+<h4>join #node.js today on irc.freenode.net!</h4>
+
+
+
+
+
+Copyright (c) 2011 Nick Baugh (niftylettuce)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
---
+++ nyancat.js
@@ -0,0 +1,111 @@
+/*
+nyancat.js
+
+Copyright (c) 2011 Nick Baugh (niftylettuce)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+var sys = require('sys'),
+    tty = require('tty');
+
+var width = tty.getWindowSize(1)[1];
+var height = tty.getWindowSize(1)[0];
+
+var colors = require('colors'),
+    flag = ".:*~*:._",
+    flagLength = flag.length,
+    numFlags = Math.floor(width / flagLength),
+    position = 1,
+    nyancat = 0,
+    color = 0; // 0 = red, 1 = yellow, 3 = green, 4 = cyan, 5 = blue, 6 = magenta)
+
+// { nyancat: 1, humanity: 0 }
+
+// .color.inverse is too hard on the eyez :'(
+function tastetherainbow(color, nyancat) {
+  switch(color) {
+    case 0:
+      sys.puts(nyancat.red);
+      break;
+    case 1:
+      sys.puts(nyancat.yellow);
+      break;
+    case 2:
+      sys.puts(nyancat.green);
+      break;
+    case 3:
+      sys.puts(nyancat.cyan);
+      break;
+    case 4:
+      sys.puts(nyancat.blue);
+      break;
+    case 5:
+      sys.puts(nyancat.magenta);
+      break;
+    default:
+      sys.puts(nyancat.rainbow);
+      break;
+  }
+}
+
+process.on('SIGINT', process.exit);
+
+// figure out probability of a unicorn KO'ing a narwhal while wrestling a nyancat
+
+
+// TODO: figure out why I can't call this function properly so it loops consistently
+// function nyancat() {
+
+for(i=0;i<100;i++) { // temporary for loop
+  for(f=1;f<numFlags + 1;f++) {
+    for(h=0;h<height-1;h++) {
+      var nyancat = "";
+      for(w=0;w<numFlags;w++) {
+        if(w === 0) {
+          nyancat += flag.substring(position, flagLength);
+        } else if (w === numFlags - 1) {
+          nyancat += (flag + flag.substring(0, position));
+        } else {
+          nyancat += flag;
+        }
+      }
+      if(color === 5)
+        color = 0;
+      else
+        color++;
+      tastetherainbow(color, nyancat);
+    }
+    if(position === flagLength) {
+      position = 1;
+    } else {
+      position++;
+    }
+  }
+}
+
+  //nyancat();
+//}
+//nyancat();
+
+var niftylettuce = "by niftylettuce | github.com/niftylettuce | @niftylettuce";
+    stagasSpaces = new Array(Math.floor((width - niftylettuce.length)/ 2)).join(" ");
+sys.puts("\n\n"+ stagasSpaces + niftylettuce.magenta+"\n\n");
+
